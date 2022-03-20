80c8ba3f7fc71bfd1c2c835ad4a292dace9a0c46
Anurag Singh <contact@anurag.sh>
2022-03-20 18:09:35 +0800

    add reference

--- v2/main.go
+++ v2/main.go
@@ -9,12 +9,17 @@ import (
 )
 
 type StatusResponse struct {
-	Emoji  string `json:"emoji"`
-	Status string `json:"status"`
+	Emoji     string `json:"emoji"`
+	Status    string `json:"status"`
+	Reference string `json:"reference"`
 }
 
 func handleRequest(ctx context.Context, event events.SQSEvent) (events.APIGatewayProxyResponse, error) {
-	resp := &StatusResponse{Emoji: "😳", Status: "Maybe"}
+	resp := &StatusResponse{
+		Emoji:     "😳",
+		Status:    "Maybe",
+		Reference: "https://en.wikipedia.org/wiki/2022_Russian_invasion_of_Ukraine",
+	}
 	// there will literally never be an error :D
 	json, _ := json.Marshal(resp)
 	return events.APIGatewayProxyResponse{Body: string(json), StatusCode: 200}, nil
